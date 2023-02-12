a2e4c3d1e74a9c95aa4056129ee9b15f1a65115a
Svetlana Lazareva <slazareva.sl@gmail.com>
2023-02-12 13:15:24 +0300

    Initial commit

--- src/main/java/ru/netology/bonus/BonusService.java
+++ src/main/java/ru/netology/bonus/BonusService.java
@@ -1,7 +1,10 @@
+package ru.netology.bonus;
+
 public class BonusService {
+
     public long calculate(long amount, boolean registered) {
         int percent = registered ? 3 : 1;
-        long bonus = amount * percent / 100;
+        long bonus = (amount * percent) / 100;
         long limit = 500;
         if (bonus > limit) {
             bonus = limit;
